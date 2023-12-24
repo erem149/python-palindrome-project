181aa3ab6be7566deb36e8876fd994c4a9a91a8e
erem149 <ryanmaulana149@gmail.com>
2023-12-24 22:26:43 +0700

    add slicing with strings

--- index.py
+++ index.py
@@ -1,1 +1,9 @@
-print("Welcome to Python 101!")
+failed_subjects = str(int(float("6.5")))
+pronouns='It\'s'
+print(pronouns + " failing " + failed_subjects + " subjects.")
+msg='Welcome to Python 101: Strings'
+print(len(msg))
+print(msg.count('o'))
+#slicing
+msg1=msg[-10] + ' ' + msg[:7] + ' ' + msg[-5:-1] + ' ' +  msg[8:10] + ' ' + msg[13] + msg[12] + msg[2] + msg[1] + msg[-5]
+print(msg1[::-1].title()) # printed "1 Welcome Ring To Tyler" as backwards
