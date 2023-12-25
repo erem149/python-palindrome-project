d9af3d5cc6bfa7ad3858faf5f271ecd522007002
erem149 <ryanmaulana149@gmail.com>
2023-12-25 07:20:49 +0700

    update index.py

--- index.py
+++ index.py
@@ -7,3 +7,5 @@ print(msg.count('o'))
 #slicing
 msg1=msg[-10] + ' ' + msg[:7] + ' ' + msg[-5:-1] + ' ' +  msg[8:10] + ' ' + msg[13] + msg[12] + msg[2] + msg[1] + msg[-5]
 print(msg1[::-1].title()) # printed "1 Welcome Ring To Tyler" as backwards
+print('Python' not in msg) # we can use not or in to describe whether it's TRUE or FALSE
+print(f'[{pronouns.capitalize()} lovely day!]')
