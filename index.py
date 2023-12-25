c95f247737cfc7db761aa37f372003e6b1f97fbf
erem149 <ryanmaulana149@gmail.com>
2023-12-25 14:32:53 +0700

    update with list challenge on index.py

--- index.py
+++ index.py
@@ -1,3 +1,4 @@
+#intro
 failed_subjects = str(int(float("6.5")))
 pronouns='It\'s'
 print(pronouns + " failing " + failed_subjects + " subjects.")
@@ -66,27 +67,57 @@ print(friends)
 print(new_friends)
 print(friends_copy)
 print(friends_list)
-# Lists - Exercise (Selling Lemonade)
-# - You sell lemonade over two weeks, the lists show number of lemonades sold per week
-# - Profit for each lemonade sold is 1.5$
-# - Add another day to week 2 list by capturing a number as input
-# - Combine the two lists into the list called 'sales'
-# - Calculate / print how much you have earned on Best Day and Worst Day
-# - Separately and in total.
-# - hint: 3 prints in total
-sales_w1 = [7,3,42,19,15,35,9]
-sales_w2 = [12,4,26,10,7,28]
-sales = []
-new_day = input('Enter #of lemonades for new day: ')
-sales_w2.append(int(new_day))
-#sales.extend(sales_w1)
-#sales.extend(sales_w2)
-print(sales_w2)
-sales = sales_w1 + sales_w2
-#sales.sort()
-print(sales)
-worst_day_prof = min(sales) * 1.5
-best_day_prof = max(sales) * 1.5
-print(f'Worst day profit:$ {worst_day_prof}')
-print(f'Best day profit:$ {best_day_prof}')
-print(f'Combined profit:$ {worst_day_prof + best_day_prof}')
+# # Lists - Exercise (Selling Lemonade)
+# # - You sell lemonade over two weeks, the lists show number of lemonades sold per week
+# # - Profit for each lemonade sold is 1.5$
+# # - Add another day to week 2 list by capturing a number as input
+# # - Combine the two lists into the list called 'sales'
+# # - Calculate / print how much you have earned on Best Day and Worst Day
+# # - Separately and in total.
+# # - hint: 3 prints in total
+# sales_w1 = [7,3,42,19,15,35,9]
+# sales_w2 = [12,4,26,10,7,28]
+# sales = []
+# new_day = input('Enter #of lemonades for new day: ')
+# sales_w2.append(int(new_day))
+# #sales.extend(sales_w1)
+# #sales.extend(sales_w2)
+# print(sales_w2)
+# sales = sales_w1 + sales_w2
+# #sales.sort()
+# print(sales)
+# worst_day_prof = min(sales) * 1.5
+# best_day_prof = max(sales) * 1.5
+# print(f'Worst day profit:$ {worst_day_prof}')
+# print(f'Best day profit:$ {best_day_prof}')
+# print(f'Combined profit:$ {worst_day_prof + best_day_prof}')
+
+#split and join
+msg = 'Welcome  to  Python  101:  Split  and  Join'
+csv = 'Eric,John,Michael,Terry,Graham'
+friends_list = ['Eric', 'John', 'Michael', 'Terry', 'Graham']
+print(list(msg), type(list(msg))) # converted the string (sentence -> letter) into list
+print(msg.split(), type(msg.split())) # converted the string (sentence -> word) into list
+print(msg.split(' '), type(msg.split(' ')))
+print(csv.split(','), type(csv.split(','))) # split type you will get is list
+print(str(friends_list), type(str(friends_list)))
+print('-'.join(friends_list), type('-'.join(friends_list))) # split type you will get is string
+print('-'.join(friends_list + friends_list), type('-'.join(friends_list + friends_list)))
+print(''.join(msg.split())) #the same result as below
+print(msg.replace(' ', '')) #the same result as above
+# - Split and Join - Exercise
+# - From the list below fill a list(friends_list) properly
+# - With the names of all the friends. One per "slot"
+# - You may need to run same command several times
+# - Use print() statements to work your way through the exercise
+csv = 'Eric,John,Michael,Terry,Graham:TerryG;Brian'
+print(csv.split(','))
+print('-'.join(csv.split(',')))
+print(','.join(csv.split(',')))
+print((','.join(csv.split(';')).split(':')))
+print(','.join(','.join(csv.split(';')).split(':')))
+print(','.join(','.join(csv.split(';')).split(':')).split(','))
+# friends_list = ['Exercise: fill me with names']
+friends_list = ','.join(','.join(csv.split(';')).split(':')).split(',')
+print(f'Result = {friends_list}')
+print('Replace = ', csv.replace(';',',').replace(':',',').split(','))
