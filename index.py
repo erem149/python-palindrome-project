cb6c048d8fe66f824eb068f6248fe8b7f0f48677
erem149 <ryanmaulana149@gmail.com>
2023-12-25 07:40:20 +0700

    update index.py

--- index.py
+++ index.py
@@ -9,3 +9,13 @@ msg1=msg[-10] + ' ' + msg[:7] + ' ' + msg[-5:-1] + ' ' +  msg[8:10] + ' ' + msg[
 print(msg1[::-1].title()) # printed "1 Welcome Ring To Tyler" as backwards
 print('Python' not in msg) # we can use not or in to describe whether it's TRUE or FALSE
 print(f'[{pronouns.capitalize()} lovely day!]')
+# - Create a distance converter converting Km to miles
+# - Take two inputs from user: Their first name and the distance in km
+# - Print: Greet user by name and show km, and mile values
+# - 1 mile is 1.609 kilometers
+# - hint: use correct types for calculating and print
+# - Did you capitalize the name
+user_name = input('Enter your name : ')
+distance_in_km = input('Enter your distance in km : ')
+distance_in_mi = float(distance_in_km) / 1.609
+print(f'Hi {user_name}! {distance_in_km} km is equivalent to {round(distance_in_mi, 1)} miles.')
