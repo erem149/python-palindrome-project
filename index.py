6609cb09832659c5cf2a0887e0ceb7002d0f85bf
erem149 <ryanmaulana149@gmail.com>
2023-12-26 14:57:49 +0700

    Update index.py

--- index.py
+++ index.py
@@ -151,7 +151,6 @@ empty_set = set()
 #4. Find names that are only in friends -> using difference and -
 #5. Show only the names who only appear in one of the lists -> using ^ and symmetric_difference
 #6. Create a new cars-list without duplicates -> using set and list
-
 friends = {'John', 'Michael', 'Terry', 'Eric', 'Graham'}
 my_friends = {'Reg', 'Loretta', 'Colin', 'John', 'Graham'}
 cars = ['900', '420', 'V70', '911', '996', 'V90', '911', '911', 'S', '328', '900']
@@ -168,3 +167,16 @@ print(my_friends ^ friends)
 cars_no_dupl = list(set(cars))
 print(cars_no_dupl)
 
+#Comments
+print("#Comments")
+#Hiding in the comments
+'''
+these are multiple comments~
+'''
+#Entry form for Ministry applications
+#to-do: fix it! it doesn't work -Fixed
+name = "Default"
+# name = input(Enter your silly name: )
+print("Thank you " + name + "!")
+print("for applying to")
+print("the Ministry of Silly Walks")
