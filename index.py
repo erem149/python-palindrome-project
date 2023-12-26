f21dd4476265edcd20313c657df768fe69ef16d2
erem149 <ryanmaulana149@gmail.com>
2023-12-27 06:10:22 +0700

    Update index.py

--- index.py
+++ index.py
@@ -180,3 +180,24 @@ name = "Default"
 print("Thank you " + name + "!")
 print("for applying to")
 print("the Ministry of Silly Walks")
+
+#Function
+# 1. Add new print statement - on a new line
+#    which says 'We hear you like the color xxx! xxx is a string with color'
+# 2. Extend the function with another input parameter 'color', that defaults to 'red'
+# 3. Capture the color via an input box as variable:color
+# 4. Change the 'You are xx!' text to say 'you will be xx+1 years old next birthday
+#    adding 1 to the age
+# 5. Capitalize first letter of the 'name', and rest are small caps
+# 6. Favorite color should be in lowercase
+def greeting(name, age=28, color='red'):
+    # Greets user with 'name' from 'input box' and 'age' next year, if variable, default age is used
+    # Also includes favorite color
+    print("Hello " + name.capitalize() + ", you will be " + str(age+1) + " years old next birthday!")
+    print(f"Hello {name.capitalize()}, you will be {age+1} next birthday!")
+    print(f'We hear you like the color {color}!')
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+color = input('Enter your favorite color: ')
+greeting(name, int(age), color)
+# greeting("Judith")
