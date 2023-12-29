25b9d2cddd575735e86cc48d72a882ab9ebb7a98
erem149 <ryanmaulana149@gmail.com>
2023-12-30 06:30:15 +0700

    Update index.py

--- index.py
+++ index.py
@@ -257,3 +257,14 @@ a = [3,7,42]
 b = [3,7,42]
 print(a is b) # Output: False
 print(id(a), id(b)) # Output: Different memory
+print(int(True)) # Output: 1
+print(int(False)) # Output: 0
+print(bool('Parrot')) # Output: True
+print(bool(' ')) # Output: True
+print(bool('')) # Output: False
+print(bool(42)) # Output: True
+print(bool(0)) # Output: False
+print(bool([1,2])) # Output: True
+print(bool([])) # Output: False
+print(42 + True) # Output: 43
+print(42 + False) # Output: 42
