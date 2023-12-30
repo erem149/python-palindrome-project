ad73115b45b2295d9c57034c931d07986d10f038
erem149 <ryanmaulana149@gmail.com>
2023-12-30 07:58:43 +0700

    Update index.py

--- index.py
+++ index.py
@@ -268,3 +268,74 @@ print(bool([1,2])) # Output: True
 print(bool([])) # Output: False
 print(42 + True) # Output: 43
 print(42 + False) # Output: 42
+
+# Conditionals: IF Statements
+is_raining = True
+is_cold = False
+print("Good Morning")
+if is_raining and is_cold:
+    print("Bring Umbrella and Jacket")
+elif is_raining and not(is_cold):
+    print("Bring Umbrella")
+elif is_cold and not(is_raining):
+    print("Bring Jacket")
+elif is_raining or is_cold:
+    print("Bring Umbrella or Jacket or both")
+else:
+    print("Shirt is Fine!")
+
+amount = 51
+if amount <= 50:
+    print("Purchase approved")
+else:
+    print("Please enter your pin!")
+# IF ELIF ELSE - Exercise
+# Create a calculator which handles +,-,*,/ and outputs answer based on the mode/ operator used
+# Hint: use 3 separate inputs
+# Bonus: Extend functionality with extra mode so it also does celcius to fahreinheit conversion
+# formula is: temp in C*9/5 + 32 = temp in F
+mode = input("\nEnter math operation (+,-,*,/) or f for Celcius to Fahrenheit conversion: ")
+num1 = float(input("Enter the first number: "))
+if (mode.lower() == 'f'):
+    print(f'{num1} Celcius is equivalent to {(num1*9/5) + 32} Fahreinheit')
+else:
+    num2 = float(input("Enter the second number: "))
+    if (mode == '+'):
+        print(f'Answer is: {num1 + num2}')
+    elif (mode == '-'):
+        print(f'Answer is: {num1 - num2}')
+    elif (mode == '*'):
+        print(f'Answer is: {num1 * num2}')
+    elif (mode == '/'):
+        print(f'Answer is: {num1 / num2}')
+    else:
+        print("Input error!")
+
+def calculator():
+    print("\n===Welcome to the Calculator===")
+    num1 = float(input("Enter the first number: "))
+    mode = input("Which math operation do you want to used (+,-,*,/): ")
+    num2 = float(input("Enter the second number: "))
+    if (mode == '+'):
+        print(f'Result: {num1 + num2}')
+    elif (mode == '-'):
+        print(f'Result: {num1 - num2}')
+    elif (mode == '*'):
+        print(f'Result: {num1 * num2}')
+    elif (mode == '/'):
+        print(f'Result: {num1 / num2}')
+    else:
+        print("Wrong input.. kindly enter the proper operator that available~")
+
+def temperature():
+    print("\n===Welcome to the Temperature Converter (Celcius to Fahreinheit)===")
+    celcius = float(input("Enter your temp in celcius: "))
+    print(f'{celcius} Celcius is equivalent to {(celcius*9/5) + 32} Fahreinheit')
+
+switch = input("\nWhat type of service do you want to use: \n1. Calculator\n2. Temperature Converter\nEnter the number 1 to 2: ")
+if(switch == '1'):
+    calculator()
+elif(switch == '2'):
+    temperature()
+else:
+    print("Wrong input.. Kindly enter the number 1 or 2~")
