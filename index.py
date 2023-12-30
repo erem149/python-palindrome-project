96e5b992fcf4c75e034306bf241e1b224d67dc5b
erem149 <ryanmaulana149@gmail.com>
2023-12-30 23:04:42 +0700

    Update index.py

--- index.py
+++ index.py
@@ -578,10 +578,42 @@ names1 = ['graHam chapman', 'TERRY', 'terry jones']
 msg = 'You are invited to the party on Saturday'
 # names.extend(names1)
 names += names1
-for index in range(2):
-    names.append(input('Enter a new name: '))
+# for index in range(2):
+#     names.append(input('Enter a new name: '))
 
 for name in names:
     msg1 = f'{name.title()}! {msg}'
     msg1 = name.title() + '! ' + msg
     print(msg1)
+
+# Enumerate This
+# Enumerate() is used with a list called l1. It first prints tuples of index and element pairs.
+# Then it changes the starting index while printing them together. Finally, it prints the index and element separately, each on its own line.
+print('Python101 - Enumerate')
+friends = ['Brian', 'Judith', 'Reg', 'Loretta', 'Colin']
+eFriends = [(51, 'Brian'), (52, 'Judith'), (53, 'Reg'), (54, 'Loretta'), (55, 'Colin')]
+i = 51
+for friend in friends:
+    print(i, friend, '\n')
+    i = i + 1 # += 1
+
+for num, friend in enumerate(friends, 51):
+    print(num, friend)
+
+for friend in enumerate(friends, 51):
+    print(friend)
+
+for friend in enumerate(enumerate(friends, 51), -100):
+    print(friend)
+
+for num, eFriend in enumerate(eFriends, 1):
+    print(num, eFriend)
+
+for num, letter in enumerate('python', start = 5):
+    print(num, letter)
+
+print(type(enumerate(friends)))
+print(list(enumerate(friends)))
+
+print(type(enumerate(eFriends)))
+print(list(enumerate(eFriends)))
