5e6c7df75e9531e1506cc744100505dc40aa5cd3
erem149 <ryanmaulana149@gmail.com>
2023-12-30 10:03:56 +0700

    Update index.py

--- index.py
+++ index.py
@@ -339,3 +339,77 @@ elif(switch == '2'):
     temperature()
 else:
     print("Wrong input.. Kindly enter the number 1 or 2~")
+
+# optimize/shorten the code in the function
+# try to reduce the number of conditionals
+def num_days(month):
+    
+    if month == 'jan':
+        print('number of days in', month, 'is', 31)
+    elif month == 'feb':
+        print('number of days in', month, 'is', 28)
+    elif month == 'mar':
+        print('number of days in', month, 'is', 31)
+    elif month == 'apr':
+        print('number of days in', month, 'is', 30)
+    elif month == 'may':
+        print('number of days in', month, 'is', 31)
+    elif month == 'jun':
+        print('number of days in', month, 'is', 30)
+    elif month == 'jul':
+        print('number of days in', month, 'is', 31)
+    elif month == 'aug':
+        print('number of days in', month, 'is', 31)
+    elif month == 'sep':
+        print('number of days in', month, 'is', 30)
+    elif month == 'oct':
+        print('number of days in', month, 'is', 31)
+    elif month == 'nov':
+        print('number of days in', month, 'is', 30)
+    elif month == 'dec':
+        print('number of days in', month, 'is', 31)
+
+num_days('jul')
+# The 1st optimization
+def num_days(month):
+    
+    if month == 'jan' or month == 'mar' or month == 'jan' or month == 'may' or month == 'jul' or month == 'aug' or month == 'oct' or month == 'dec':
+        print('number of days in', month, 'is', 31)
+    elif month == 'feb':
+        print('number of days in', month, 'is', 28)
+    elif month == 'apr' or month == 'jun' or month == 'sep' or month == 'nov':
+        print('number of days in', month, 'is', 30)
+
+num_days('jan')
+# The 2nd optimization
+def num_days(month):
+    
+    if month == 'jan' or month == 'mar' or month == 'jan' or month == 'may' or month == 'jul' or month == 'aug' or month == 'oct' or month == 'dec':
+        print('number of days in', month, 'is', 31)
+    elif month == 'feb':
+        print('number of days in', month, 'is', 28)
+    else:
+        print('number of days in', month, 'is', 30)
+
+num_days('oct')
+# The 3rd optimization
+def num_days(month):
+    days = 31
+    if month == 'apr' or month == 'jun' or month == 'sep' or month == 'nov':
+        days = 30
+    elif month == 'feb':
+        days = 28
+    print('number of days in', month, 'is', days)
+
+num_days('feb')
+# The 4th optimization
+def num_days(month):
+    days = 31
+    if month in ['apr', 'jun', 'sep', 'nov']: # we can also use list, tuple and set. Set is a bit faster among those three membership functions.
+    # if month == 'apr' or month == 'jun' or month == 'sep' or month == 'nov':
+        days = 30
+    elif month == 'feb':
+        days = 28
+    print('number of days in', month, 'is', days)
+
+num_days('sep')
