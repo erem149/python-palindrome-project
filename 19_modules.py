b39ac09a6f05dfd89e6412168e6e06cf28ee7de6
erem149 <ryanmaulana149@gmail.com>
2024-01-01 23:17:42 +0700

    Added 19_modules.py

---
+++ 19_modules.py
@@ -0,0 +1,16 @@
+#Modules
+print('Modules')
+# datetime random string os math browser -> some of modules available in python
+# import platform, string, os
+# import platform as pl
+# from platform import python_version, system
+from platform import python_version as pv, system
+
+# print(dir(platform))
+# print(platform.python_version())
+# print(pl.python_version())
+# print(python_version())
+print(pv())
+print(system())
+
+# reference: https://docs.python.org/3/py-modindex.html
