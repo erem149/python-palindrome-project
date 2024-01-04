27a739871d56b871c1d8a29da0a1bfae86042c99
erem149 <ryanmaulana149@gmail.com>
2024-01-05 04:58:40 +0700

    Added 25_timeit_performance.py

---
+++ 25_timeit_performance.py
@@ -0,0 +1,28 @@
+#  Timeit and Performance
+print('Performance and Timeit module')
+import timeit
+# Experiment with sieve of Eratosthenes
+def test1():
+    [x for x in range(1, 151) if not any([x % y == 0 for y in range(2, x)]) and not x ==1 ]
+    return(1)
+def test2():
+    [x for x in range(2, 151) if not any([x % y == 0 for y in range(2, x)])]
+
+#Initialize a list
+def test3():
+    primes = []
+    for possiblePrime in range(2, 151):
+    # Assume number is prime until shown it is not.
+        isPrime = True
+        for num in range(2, int(possiblePrime ** 0.5) + 1):
+            if possiblePrime % num == 0:
+                isPrime = False
+                break
+        if isPrime:
+            primes.append(possiblePrime)
+    # print(primes)
+    return(1)
+    
+print(timeit.timeit('test1()', globals=globals(), number = 190))
+print(timeit.timeit('test2()', globals=globals(), number = 190))
+print(timeit.timeit('test3()', globals=globals(), number = 190))
